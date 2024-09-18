1e7b99abb7cd557791cfb4b0095b1f5e32917846
unknown <114921992+dl960@users.noreply.github.com>
2024-09-18 09:55:56 +0100

    Revert "Added Hidden Future Files"
    This reverts commit 2c3ba6a12b8fe822486a74a1720f2c2221e712ff.

--- Lectures/W2L1 examples/mouseCircle/sketch.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-    createCanvas(400, 300);
-}
-
-function draw() {
-    background(0);
-    // mouseX and mouseY are variables that store the current coordinates of the mouse
-    circle(mouseX, mouseY, 40);
-}
